58052de4900d9ac5550cc3896740eb4814845f10
sharpszy <whosharp007@gmail.com>
2023-03-17 12:07:26 +0800

    q20 有效的括号

---
+++ leetcode/goimpl/simple/q20_valid_parentheses_test.go
@@ -0,0 +1,63 @@
+package simple
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Q20: 有效的括号
+// https://leetcode.cn/problems/valid-parentheses/
+// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+// 有效字符串需满足：
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// 每个右括号都有一个对应的相同类型的左括号。
+// 补充：括号可以嵌套
+//
+// 示例 1：
+// 输入：s = "()"
+// 输出：true
+//
+// 示例 2：
+// 输入：s = "()[]{}"
+// 输出：true
+//
+// 示例 3：
+// 输入：s = "{[()]}"
+// 输出：true
+
+func validParentheses(s string) bool {
+	l := len(s)
+	if l%2 == 1 {
+		return false
+	}
+
+	pairs := map[byte]byte{'(': ')', '[': ']', '{': '}'}
+
+	stack := make([]byte, 0)
+	for _, c := range []byte(s) {
+		if v, ok := pairs[c]; ok {
+			stack = append(stack, v)
+		} else if len(stack) > 0 && stack[len(stack)-1] == c {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
+
+func Test_validParentheses(t *testing.T) {
+	str := "()"
+	assert.True(t, validParentheses(str))
+
+	str = "()[]{}"
+	assert.True(t, validParentheses(str))
+
+	str = "{[()]}"
+	assert.True(t, validParentheses(str))
+
+	str = "{{[]]}"
+	assert.False(t, validParentheses(str))
+}
