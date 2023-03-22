6541d9bb2fd7ad693ec4694fee941db5eb51518c
sharpszy <whosharp007@gmail.com>
2023-03-22 10:45:49 +0800

    Q11 盛最多水的容器-最优解双指针

--- leetcode/goimpl/middle/q11_max_area_test.go
+++ leetcode/goimpl/middle/q11_max_area_test.go
@@ -7,25 +7,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// 暴力解法
 func maxArea(height []int) int {
 	l := len(height)
 	if l < 2 {
 		return 0
 	}
 
-	max := 0
+	area := 0
 	for i := 0; i < l; i++ {
 		for j := l - 1; j > i; j-- {
-			max = common.Max(max, common.Min(height[i], height[j])*(j-i))
+			area = common.Max(area, common.Min(height[i], height[j])*(j-i))
 		}
 	}
-	return max
+	return area
+}
+
+// 最优解法：双指针
+func maxArea2(height []int) int {
+	l, r := 0, len(height)-1
+	area := 0
+	for l < r {
+		area = common.Max(area, (r-l)*common.Min(height[r], height[l]))
+		// 移动高度较小的那一端
+		if height[l] < height[r] {
+			l++
+		} else {
+			r--
+		}
+	}
+	return area
 }
 
 func Test_maxArea(t *testing.T) {
 	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
 	assert.Equal(t, 49, maxArea(height))
+	assert.Equal(t, 49, maxArea2(height))
 
 	height = []int{1, 1}
 	assert.Equal(t, 1, maxArea(height))
+	assert.Equal(t, 1, maxArea2(height))
 }
