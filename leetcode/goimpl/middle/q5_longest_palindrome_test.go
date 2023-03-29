888df09a4339f4bbbdbf8a5adf71a5e765cc53d0
sharpszy <whosharp007@gmail.com>
2023-03-29 09:49:26 +0800

    Q5 最长回文子串-中心扩展算法

--- leetcode/goimpl/middle/q5_longest_palindrome_test.go
+++ leetcode/goimpl/middle/q5_longest_palindrome_test.go
@@ -46,17 +46,52 @@ func isPalindrome(s *string, start, end int) bool {
 	return true
 }
 
+// 中心扩展算法
+func longestPalindrome2(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		// 情形一
+		left1, right1 := expandCenter(s, i, i)
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+
+		// 情形二
+		left2, right2 := expandCenter(s, i, i+1)
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandCenter(s string, left, right int) (int, int) {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return left + 1, right - 1
+}
+
 func Test_longestPalindrome(t *testing.T) {
 
 	s := "babad"
 	assert.Equal(t, "bab", longestPalindrome(s))
+	assert.Equal(t, "bab", longestPalindrome2(s))
 
 	s = ""
 	assert.Equal(t, "", longestPalindrome(s))
+	assert.Equal(t, "", longestPalindrome2(s))
 
 	s = "bb"
 	assert.Equal(t, "bb", longestPalindrome(s))
+	assert.Equal(t, "bb", longestPalindrome2(s))
 
 	s = "cbbd"
 	assert.Equal(t, "bb", longestPalindrome(s))
+	assert.Equal(t, "bb", longestPalindrome2(s))
 }
